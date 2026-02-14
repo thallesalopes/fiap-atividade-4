fd243e4503d003b52d34c980bb8d21348d22fe15
Thalles Ferreira <thallesalopes@gmail.com>
2026-02-13 21:53:58 -0300

    .

---
+++ src/main/java/com/fiap/fiap_atividade_4/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.fiap.fiap_atividade_4;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FiapAtividade4Application.class);
+	}
+
+}
